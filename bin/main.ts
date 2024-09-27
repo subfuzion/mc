4c9374e6f0d32f011480a1866359f2318fafe22f
Tony Pujals <subfuzion@users.noreply.github.com>
2024-09-27 10:54:41 -0700

    minor: update comment

--- bin/main.ts
+++ bin/main.ts
@@ -7,7 +7,7 @@ import {IO} from '#lib/cli/io.ts';
 try {
   await Cli.run(argv);
 } catch (err) {
-  // ALL early CLI exceptions (until a command runs) surface here.
+  // All exceptions during CLI initialization surface here.
   IO.abort(err);
   process.exitCode = 1;
 }
