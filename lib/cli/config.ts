9160165b4301b71cd57fb45702ee52b696d4f956
Tony Pujals <subfuzion@users.noreply.github.com>
2024-09-30 11:51:40 -0700

    config

--- lib/cli/config.ts
+++ lib/cli/config.ts
@@ -2,10 +2,16 @@ import { parse } from "jsr:@std/yaml";
 
 const DefaultConfigFile = "mc.config.yaml";
 
-export class Config {
-  name?: string;
+export interface Config {
+  name?: string
   description?: string;
   version?: string;
+}
+
+export class Config {
+  name?: string = "mc";
+  description?: string = "run a suite of one or more programs and report failures";
+  version?: string = "0.0.0";
 
   constructor(yaml: string) {
     const config = parse(yaml) as Config;
